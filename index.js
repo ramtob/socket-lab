e5e5788cadb8a2d1bb938f10f93b240121d04fee
ramtob <ramtob@gmail.com>
2016-05-24 17:59:34 +0300

    just another commit

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-	var msg = "Server: socket " +  socket.id + " connected";
+io.on('connection', function (socket) {
+	var msg = "Server: socket " + socket.id + " connected";
 	console.log(msg);
 	io.emit('chat message', msg);
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', function(){
-    msg = "Server: socket "+socket.id+" disconnected";
-	console.log(msg);
-  });
+	socket.on('chat message', function (msg) {
+		io.emit('chat message', msg);
+	});
+	socket.on('disconnect', function () {
+		msg = "Server: socket " + socket.id + " disconnected";
+		console.log(msg);
+	});
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var nsp = io.of('/nsp');
+nsp.on('connection', function(socket){
+  var msg = "Server: nsp socket " + socket.id + " connected";
+	console.log(msg);
+	socket.on('disconnect', function () {
+		msg = "Server: nsp socket " + socket.id + " disconnected";
+		console.log(msg);
+	});
+});
+
+http.listen(3000, function () {
+	console.log('listening on *:3000');
 });
